234797b268d0c6641eb04c98d032379542fad459
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-08-11 16:15:47 +0300

    added PetTypes to bootstrap

--- pet-clinic-web/src/main/java/mcgurbuz/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mcgurbuz/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,21 +4,32 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import mcgurbuz.springframework.sfgpetclinic.services.*;
 import mcgurbuz.springframework.sfgpetclinic.model.*;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetServiceType petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService){
+    public DataLoader(OwnerService ownerService, VetService vetService, PetServiceType petTypeService){
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog= new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat= new PetType();
+        cat.setName("Dog");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1= new Owner();
         owner1.setFirstName("Michael");
