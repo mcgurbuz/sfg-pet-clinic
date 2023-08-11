e77403cab4461e0be3264b2646132607432a759b
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-08-11 15:18:09 +0300

    added visit,updated owner with pet relationship.

--- pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,17 @@
 package mcgurbuz.springframework.sfgpetclinic.model;
 
 import mcgurbuz.springframework.sfgpetclinic.model.Person;
+
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package mcgurbuz.springframework.sfgpetclinic.model;
 
 public class PetType extends BaseEntity{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
