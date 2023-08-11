22981c00cb778c4d4e7a09824de6f43a4d4baa13
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-08-11 16:01:23 +0300

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/services/PetServiceType.java
@@ -0,0 +1,6 @@
+package mcgurbuz.springframework.sfgpetclinic.services;
+
+import mcgurbuz.springframework.sfgpetclinic.model.PetType;
+public interface PetServiceType extends CrudService<PetType,Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,42 @@
+package mcgurbuz.springframework.sfgpetclinic.services.map;
+
+import mcgurbuz.springframework.sfgpetclinic.model.PetType;
+import mcgurbuz.springframework.sfgpetclinic.services.PetServiceType;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetServiceType {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+
+
+}
