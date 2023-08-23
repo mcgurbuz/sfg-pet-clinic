8c0e65c693243f54c136cbbe698ae2075d1ffbf0
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-08-23 15:32:16 +0300

    Updated vet to use JPA

--- pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package mcgurbuz.springframework.sfgpetclinic.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
