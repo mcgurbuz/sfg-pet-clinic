28b161746df3ddc9f957480dd3bc7b36160bf27c
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-08-29 13:42:05 +0300

    Imp Map imp for  VisitServicee

---
+++ pet-clinic-data/src/main/java/mcgurbuz/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package mcgurbuz.springframework.sfgpetclinic.services;
+
+import mcgurbuz.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+
+}
