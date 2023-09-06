277576e5c536834a12de9dbc8edc978e9e245fa2
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-09-07 00:28:16 +0300

    Write CRUD Test for Owner Map Service

--- pet-clinic-web/src/test/java/mcgurbuz/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/mcgurbuz/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,6 +3,7 @@ package mcgurbuz.springframework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
