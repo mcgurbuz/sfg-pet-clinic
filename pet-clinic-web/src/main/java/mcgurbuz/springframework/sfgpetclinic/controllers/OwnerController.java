9c57edc606efd2cbd0e785a0f242f50f0cb6128d
mustafacem.gurbuz <Mustafacem.Gurbuz@logo.com.tr>
2023-09-08 02:49:52 +0300

    added  MockMvc Tests for Owner Controller

--- pet-clinic-web/src/main/java/mcgurbuz/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/mcgurbuz/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import mcgurbuz.springframework.sfgpetclinic.services.*;
 
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
---
+++ pet-clinic-web/src/test/java/mcgurbuz/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,78 @@
+package mcgurbuz.springframework.sfgpetclinic.controllers;
+
+import mcgurbuz.springframework.sfgpetclinic.model.Owner;
+import mcgurbuz.springframework.sfgpetclinic.services.OwnerService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
+
+    MockMvc mockMvc; //Mock için sahte kurulan bir yapı MVC yapısı
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1l).build());
+        owners.add(Owner.builder().id(2l).build());
+
+        mockMvc = MockMvcBuilders.
+                standaloneSetup(controller)
+                .build(); //Her test ortamında sahte bir controller ortamı oluşturacaktır.
+
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void listOwnersByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void findOwners() throws Exception {
+
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notimplemented"));
+
+        //verifyZeroInteractions(ownerService);
+
+
+    }
+}
